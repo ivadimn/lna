7da758aa5ebf227c4558034cc257166bfcda9a66
Vadim Ivanov <ivadimn@mail.ru>
2022-02-07 17:52:57 +0300

    update module lna

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PyQt6.QtWidgets import QApplication
+from PyQt6 import QtSql
+
 
 from ui.MainWindow import MainWindow
 
@@ -6,7 +8,21 @@ app = QApplication([])
 window = MainWindow()
 window.show()
 
+conn = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+conn.setDatabaseName("lna.db")
+conn.open()
+query = QtSql.QSqlQuery()
+
+query.exec("Select name From org_units;")
+query.first()
+while query.isValid():
+    print(query.value(0))
+    query.next()
+
+conn.close()
 app.exec()
+
+
 
 
 
